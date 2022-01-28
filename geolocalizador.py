97a30b60b3066f1cfe2f77f74abc65b5315776e0
DevMarcelo <48558530+MarceloNogueira1@users.noreply.github.com>
2022-01-28 12:16:58 -0300

    Update geolocalizador.py

--- geolocalizador.py
+++ geolocalizador.py
@@ -1,10 +1,13 @@
 import requests 
 
+
+
 def start_api(cep):
     global adress_data
     r = requests.get(f'https://ipgeolocation.abstractapi.com/v1/?api_key=a5832ca9a0264f00b77ab38ef833085a&ip_address={cep}')
     adress_data = r.json()
 
+    
 def resultados():
     print('IP:',adress_data["ip_address"])
     print('CIDADE:', adress_data["city"])
@@ -21,6 +24,7 @@ def resultados():
     print('PROVEDOR:', adress_data["connection"]["autonomous_system_organization"])
     print('TIPO DE CONEXÃO:', adress_data["connection"]["connection_type"])
 
+    
 ip = input('DIGITE O IP A SER CONSULTADO: ')
 
 start_api(cep=ip)
